daacefd93b7d73cdb8aa3196215d2ff97d4780e5
github <noreply@github.com>
2022-05-24 11:18:00 +0000

    1;

--- src/fp16tofp32.py
+++ src/fp16tofp32.py
@@ -1,8 +1,27 @@
-import onnx_graphsurgeon as gs
 import onnx
 from onnx import helper, AttributeProto, TensorProto, GraphProto, numpy_helper
 import numpy as np
 
+in_onnx_file = "/workspaces/deort/onnx/fp16.onnx"
+out_onnx_file = "/workspaces/deort/onnx/fp32.onnx"
+
+fp16_model = onnx.load_model(in_onnx_file)
+
+# change all initializer from fp16 to fp32
+fp16_init_name = set()
+fp32_init = []
+for init in fp16_model.graph.initializer:
+    if init.data_type == TensorProto.FLOAT16:
+        print(init.name, init.dims)
+        fp16_init_name.add(init.name)
+        fp32_data = numpy_helper.to_array(init)
+        if len(init.dims) == 0:
+            fp32_data = [fp32_data.tolist()]
+        fp32_tensor_init = onnx.helper.make_tensor(
+            name= init.name,
+            data_type=TensorProto.FLOAT,
+            dims= init.dims,
+            vals= fp32_data)
 
 
 if __name__ == "__main__":
